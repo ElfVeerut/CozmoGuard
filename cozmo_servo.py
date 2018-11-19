cb5c960c9df2039413cfce5839126957b151cedd
cozmoguard <42993063+cozmoguard@users.noreply.github.com>
2018-11-19 17:34:44 +0700

    Add files via upload

--- cozmo_servo.py
+++ cozmo_servo.py
@@ -1,3 +1,4 @@
+#FLASK_APP=cozmo_servo.py flask run
 #import RPi.GPIO as GPIO
 import time
 from flask import Flask
@@ -8,6 +9,35 @@ app = Flask(__name__)
 #    servoPIN = 17
 #    GPIO.setmode(GPIO.BCM)
 #    GPIO.setup(servoPIN, GPIO.OUT)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        username = session['username']
+            return 'Logged in as ' + username + '<br>' + \
+                "<b><a href = '/logout'>click here to log out</a></b>"
+                    return "You are not logged in <br><a href = '/login'></b>" + \
+                        "click here to log in</b></a>"
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+            return '''
+                
+                <form action = "" method = "post">
+                <p><input type = text name = username/></p>
+                <p<<input type = submit value = Login/></p>
+                </form>
+                
+                '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it is there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route('/cozmoguardOpen')
 def cozmoguardOpen():
